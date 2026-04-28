ae82801617137c53a75ecb18a67300d847ea43c5
Michael Skipper <mskipper@crcfe01.crc.nd.edu>
2026-04-28 17:08:29 -0400

    Added input to quickstart for number of workers, generates random 20 moves (with set seed) before submitting tasks

--- quickstart.py
+++ quickstart.py
@@ -3,6 +3,8 @@
 import ndcctools.taskvine as vine
 import pyffish
 import time
+import random
+import sys
 
 def evaluate_fen_worker(variant, fen_to_evaluate, engine_path, depth):
     """
@@ -66,60 +68,72 @@ def evaluate_fen_worker(variant, fen_to_evaluate, engine_path, depth):
         if process:
             process.terminate()
 
-# Create a new manager
-m = vine.Manager(name=f"duckchess-{int(time.time())}")
-m.enable_monitoring()
-print(f"Listening on port {m.port}")
+def run_tasks(num_workers):
+    # Create a new manager
+    m = vine.Manager(name=f"duckchess-{int(time.time())}")
+    m.enable_monitoring()
+    print(f"Listening on port {m.port}")
 
-num_workers = 10
+    workers = vine.Factory("condor", m)
+    workers.max_workers = num_workers
+    workers.cores = 1
+    workers.memory = 1000
+    workers.disk = 1000
 
-workers = vine.Factory("condor", m)
-workers.max_workers = num_workers
-workers.cores = 1
-workers.memory = 1000
-workers.disk = 1000
+    f = m.declare_file("./stockfish")
 
-f = m.declare_file("./stockfish")
+    variant = "duck"
+    current_fen = pyffish.start_fen(variant)
 
-variant = "duck"
-current_fen = pyffish.start_fen(variant)
-moves = pyffish.legal_moves(variant, current_fen, [])
-n = len(moves)
-submit_time = time.time()
-# Submit several tasks for execution:
-with workers:
-    print("submitting tasks...")
-    for move in moves:
-        resulting_fen = pyffish.get_fen(variant, current_fen, [move])
-        task = vine.PythonTask(evaluate_fen_worker, variant, resulting_fen, "./stockfish", 4)
-        task.add_input(f, "./stockfish")
-        task.set_cores(1)
-        m.submit(task)
+    for i in range(20):
+        moves = pyffish.legal_moves(variant, current_fen, [])
+        if not moves: 
+            break
+        current_fen = pyffish.get_fen(variant, current_fen, [random.choice(moves)]) 
+
+    moves = pyffish.legal_moves(variant, current_fen, [])
+    n = len(moves)
+    submit_time = time.time()
+    # Submit several tasks for execution:
+    with workers:
+        print("submitting tasks...")
+        for move in moves:
+            resulting_fen = pyffish.get_fen(variant, current_fen, [move])
+            task = vine.PythonTask(evaluate_fen_worker, variant, resulting_fen, "./stockfish", 4)
+            task.add_input(f, "./stockfish")
+            task.set_cores(1)
+            m.submit(task)
     
-    wait_time = time.time()
-    first_time = None
+        wait_time = time.time()
+        first_time = None
 
-    # As they complete, display the results:
-    print("waiting for tasks to complete...")
-    while not m.empty():
-        task = m.wait(5)
-        if task:
-            if first_time is None:
-                first_time = time.time()
-            # print("task {} completed with result {}".format(task.id, task.output))
-    done_time = time.time()
-    print("{} workers: {} tasks completed".format(num_workers, n))
+        # As they complete, display the results:
+        print("waiting for tasks to complete...")
+        while not m.empty():
+            task = m.wait(5)
+            if task:
+                if first_time is None:
+                    first_time = time.time()
+                # print("task {} completed with result {}".format(task.id, task.output))
+        done_time = time.time()
+        print("{} workers: {} tasks completed".format(num_workers, n))
     
-    total = done_time - submit_time
-    rate = n / (total * 1.0)
-    print(f"\tTotal time: {total:.3f} seconds ({rate:.3f} tasks/sec)")
+        total = done_time - submit_time
+        rate = n / (total * 1.0)
+        print(f"\tTotal time: {total:.3f} seconds ({rate:.3f} tasks/sec)")
 
-    total = done_time - wait_time
-    rate = n / (total * 1.0)
-    print(f"\tTime excluding submission: {total:.3f} seconds ({rate:.3f} tasks/sec)")
+        total = done_time - wait_time
+        rate = n / (total * 1.0)
+        print(f"\tTime excluding submission: {total:.3f} seconds ({rate:.3f} tasks/sec)")
 
-    total = done_time - first_time
-    rate = n / (total * 1.0)
-    print(f"\tTime after first result: {total:.3f} seconds ({rate:.3f} tasks/sec)")
+        total = done_time - first_time
+        rate = n / (total * 1.0)
+        print(f"\tTime after first result: {total:.3f} seconds ({rate:.3f} tasks/sec)")
 
-print("all done.")
+    print("all done.")
+
+if __name__ == "__main__":
+    num_workers = int(sys.argv[1])
+    random.seed(42)
+
+    run_tasks(num_workers)
